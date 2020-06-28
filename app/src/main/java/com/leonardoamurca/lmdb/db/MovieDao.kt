d7446fb048c5c08655e216b1185315f17b5379be
Leonardo Murça <leonardoamurca@gmail.com>
2020-06-28 10:24:15 -0300

    Add entity and dao for Movie

---
+++ app/src/main/java/com/leonardoamurca/lmdb/db/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.leonardoamurca.lmdb.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MovieDao {
+
+    @Insert
+    fun insert(movie: MovieEntity)
+
+    @Query("SELECT * FROM movies WHERE id = :id")
+    fun getMovieBy(id: Int): MovieEntity
+
+    @Query("SELECT * FROM movies")
+    fun getMovies(): List<MovieEntity>
+}
---
+++ app/src/main/java/com/leonardoamurca/lmdb/db/MovieEntity.kt
@@ -0,0 +1,30 @@
+package com.leonardoamurca.lmdb.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = MovieEntity.TABLE_NAME)
+data class MovieEntity(
+    @ColumnInfo(name = FIELD_ID)
+    @PrimaryKey
+    val id: Int,
+    @ColumnInfo(name = FIELD_TITLE)
+    val title: String,
+    @ColumnInfo(name = FIELD_OVERVIEW)
+    val overview: String,
+    @ColumnInfo(name = FIELD_VOTE_AVERAGE)
+    val voteAverage: Float,
+    @ColumnInfo(name = FIELD_POSTER_IMAGE)
+    val posterImage: String
+) {
+
+    companion object {
+        const val TABLE_NAME = "movies"
+        private const val FIELD_ID = "id"
+        private const val FIELD_TITLE = "title"
+        private const val FIELD_VOTE_AVERAGE = "vote_average"
+        private const val FIELD_OVERVIEW = "overview"
+        private const val FIELD_POSTER_IMAGE = "poster_image"
+    }
+}
