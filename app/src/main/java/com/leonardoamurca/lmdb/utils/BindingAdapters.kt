f71d2cf3f41ae70621ffeae49684f6e9821cacd5
Leonardo Murça <leonardoamurca@gmail.com>
2020-07-04 10:43:21 -0300

    Add movie layout with image fetch binding adapter

---
+++ app/src/main/java/com/leonardoamurca/lmdb/utils/BindingAdapters.kt
@@ -0,0 +1,19 @@
+package com.leonardoamurca.lmdb.utils
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.leonardoamurca.lmdb.R
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
+}
